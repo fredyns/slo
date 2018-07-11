ae2d4899d9a72f610cd198b0c2575aaad1631639
Fredy Nurman Saleh <email@fredyns.net>
2018-07-11 10:05:56 +0700

    init host config

--- web/index.php
+++ web/index.php
@@ -1,12 +1,16 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+// init script
+$init = __DIR__.'/../config/host/init.php';
+if (file_exists($init)) {
+    require $init;
+}
 
+// load config
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 
+// run app
 (new yii\web\Application($config))->run();
