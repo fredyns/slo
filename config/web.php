4c06c0c83c161007c9c47afcaf83b8618cd31e73
Fredy Nurman Saleh <email@fredyns.net>
2018-07-12 09:05:34 +0700

    add kartik grid

--- config/web.php
+++ config/web.php
@@ -56,7 +56,15 @@ $config = [
         ],
         'admin' => [
             'class' => 'mdm\admin\Module',            
-        ]
+        ],
+        'gridview' => [
+            'class' => '\kartik\grid\Module'
+            // enter optional module parameters below - only if you need to  
+            // use your own export download action or custom translation 
+            // message source
+            // 'downloadAction' => 'gridview/export/download',
+            // 'i18n' => []
+        ],
     ],
     'params' => host_config('params.php'),
     'as access' => [
