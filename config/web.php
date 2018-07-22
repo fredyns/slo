e27bb4b0a8caccbc08e12627a21e843f348838d3
Fredy Nurman Saleh <email@fredyns.net>
2018-07-22 09:23:31 +0700

    as_access config

--- config/web.php
+++ config/web.php
@@ -67,18 +67,7 @@ $config = [
         ],
     ],
     'params' => host_config('params.php'),
-    'as access' => [
-        'class' => 'mdm\admin\components\AccessControl',
-        'allowActions' => [
-            'site/*',
-            'admin/*',
-            // The actions listed here will be allowed to everyone including guests.
-            // So, 'admin/*' should not appear here in the production, of course.
-            // But in the earlier stages of your development, you may probably want to
-            // add a lot of actions here until you finally completed setting up rbac,
-            // otherwise you may not even take a first step.
-        ]
-    ],
+    'as access' => host_config('as_access.php'),
 ];
 
 if (YII_ENV_DEV) {
