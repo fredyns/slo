1754b4f30991b05fb37fddfd230386e754214dad
Fredy Nurman Saleh <email@fredyns.net>
2018-08-03 11:10:00 +0700

    [WIP] translation

---
+++ modules/location/messages/id/app.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'Translation' => 'Terjemah',
+    'Type any place' => 'Ketik suatu tempat',
+    'Search' => 'Cari',
+    'Select' => 'Pilih',
+    'Sublocation' => 'Sublokasi',
+];
