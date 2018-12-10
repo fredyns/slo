fae11d70e4ffb16af7e52037a136977ce26eda19
Fredy Nurman Saleh <email@fredyns.net>
2018-12-10 14:54:03 +0700

    status dict

---
+++ dictionaries/SubmissionProgressStatus.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\dictionaries;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * Description of SubmissionProgressStatus
+ *
+ * @author Fredy Nurman Saleh <email@fredyns.net>
+ */
+abstract class SubmissionProgressStatus
+{
+    const REQUEST = 0;
+    const REGISTRATION = 50;
+    const REGISTERED = 100;
+
+    /**
+     * @return array
+     */
+    public static function labels()
+    {
+        return [
+            static::REQUEST => Yii::t('dictionaries', 'Request'),
+            static::REGISTRATION => Yii::t('dictionaries', 'Registration'),
+            static::REGISTERED => Yii::t('dictionaries', 'Registered'),
+        ];
+    }
+
+    /**
+     * @param int $value
+     * @return string
+     */
+    public static function getLabel($value)
+    {
+        return ArrayHelper::getValue(static::labels(), $value, $value);
+    }
+
+}
