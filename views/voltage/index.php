1a7072d0bcf6a4ab3af12a47a7c3d9aea0d26f0d
Fredy Nurman Saleh <email@fredyns.net>
2018-12-17 21:32:46 +0700

    generate CRUD for Voltage

---
+++ views/voltage/index.php
@@ -0,0 +1,100 @@
+<?php
+
+use app\models\Voltage;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+/* @var $searchModel app\models\VoltageSearch */
+
+$this->title = $searchModel->getAliasModel(TRUE);
+$this->params['breadcrumbs'][] = $this->title;
+
+Yii::$app->view->params['pageButtons'] = Html::a('<span class="glyphicon glyphicon-plus"></span> '.Yii::t('cruds', 'New'), ['create'], ['class' => 'btn btn-success']);
+$actionColumnTemplateString = "{view} {update}";
+
+$actionColumnTemplateString = '<div class="action-buttons">'.$actionColumnTemplateString.'</div>';
+?>
+<div class="giiant-crud voltage-index">
+
+    <h1>
+        <?= $searchModel->getAliasModel(TRUE) ?>
+        <small>
+            <?= Yii::t('cruds', 'List') ?>
+        </small>
+    </h1>
+
+    <div class="clearfix crud-navigation">
+        <div class="pull-left">
+        </div>
+
+        <div class="pull-right">
+            <?= Html::a('<span class="glyphicon glyphicon-plus"></span> '.Yii::t('cruds', 'New'), ['create'], ['class' => 'btn btn-success']) ?>
+        </div>
+    </div>
+
+    <?php // $this->render('_search', ['model' =>$searchModel]); ?>
+
+    <hr />
+
+    <?php
+    \yii\widgets\Pjax::begin([
+        'id' => 'pjax-main',
+        'enableReplaceState' => false,
+        'linkSelector' => '#pjax-main ul.pagination a, th a',
+        'clientOptions' => [
+            'pjax:success' => 'function(){alert("yo")}',
+        ],
+    ]);
+    ?>
+
+    <div class="table-responsive">
+        <?= 
+        GridView::widget([
+            'dataProvider' => $dataProvider,
+            'pager' => [
+                'class' => yii\widgets\LinkPager::className(),
+                'firstPageLabel' => Yii::t('cruds', 'First'),
+                'lastPageLabel' => Yii::t('cruds', 'Last'),
+            ],
+            'filterModel' => $searchModel,
+            'tableOptions' => ['class' => 'table table-striped table-bordered table-hover'],
+            'headerRowOptions' => ['class' => 'x'],
+            'columns' => [
+                [
+                    'class' => 'yii\grid\SerialColumn',
+                ],
+                'name',
+                [
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => $actionColumnTemplateString,
+                    'buttons' => [
+                        'view' => function ($url, $model, $key) {
+                            $options = [
+                                'title' => Yii::t('cruds', 'View'),
+                                'aria-label' => Yii::t('cruds', 'View'),
+                                'data-pjax' => '0',
+                            ];
+                            return Html::a('<span class="glyphicon glyphicon-file"></span>', $url, $options);
+                        }
+                    ],
+                    'urlCreator' => function($action, $model, $key, $index) {
+                        // using the column name as key, not mapping to 'id' like the standard generator
+                        $params = is_array($key) ? $key : [$model->primaryKey()[0] => (string) $key];
+                        $params[0] = Yii::$app->controller->id ? Yii::$app->controller->id.'/'.$action : $action;
+                        return Url::toRoute($params);
+                    },
+                    'contentOptions' => ['nowrap' => 'nowrap']
+                ],
+            ],
+        ]);
+        ?>
+    </div>
+
+    <?php \yii\widgets\Pjax::end() ?>
+
+</div>
+
