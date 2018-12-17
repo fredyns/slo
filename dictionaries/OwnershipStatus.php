9f6d0c3fd918570a1d506294efd7c9be3ba756c5
Fredy Nurman Saleh <email@fredyns.net>
2018-12-17 21:25:34 +0700

    create OwnershipStatus dictionary

---
+++ dictionaries/OwnershipStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\dictionaries;
+
+/**
+ * Description of OwnershipStatus
+ *
+ * @author Fredy Nurman Saleh <email@fredyns.net>
+ */
+class OwnershipStatus
+{
+    const OWNED_BY_STATE_ENTERPRISE = 1;
+    const OWNED_BY_PRIVATE = 2;
+    const RENT_FROM_STATE_ENTERPRISE = 3;
+    const RENT_FROM_PRIVATE = 4;
+
+    /**
+     * @return array
+     */
+    public static function all()
+    {
+        return [
+            static::RENT_FROM_STATE_ENTERPRISE => Yii::t('dictionaries', 'Rent From State Enterprise'),
+            static::RENT_FROM_PRIVATE => Yii::t('dictionaries', 'Rent From Private Company'),
+            static::OWNED_BY_STATE_ENTERPRISE => Yii::t('dictionaries', 'Owned By State Enterprise'),
+            static::OWNED_BY_PRIVATE => Yii::t('dictionaries', 'Owned By Private Company'),
+        ];
+    }
+
+    /**
+     * @param int $value
+     * @return string
+     */
+    public static function getLabel($value)
+    {
+        return ArrayHelper::getValue(static::all(), $value, $value);
+    }
+
+}
