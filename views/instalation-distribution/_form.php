4d358a9edf089cd795b75da527c9e579eb958dd3
Fredy Nurman Saleh <email@fredyns.net>
2018-12-18 16:21:57 +0700

    build view widget for inst. distribution

--- views/instalation-distribution/_form.php
+++ views/instalation-distribution/_form.php
@@ -12,119 +12,58 @@ use dmstr\bootstrap\Tabs;
 
 <div class="instalation-distribution-form">
 
-    <?php
-    $form = ActiveForm::begin([
-            'id' => 'InstalationDistribution',
-            'layout' => 'horizontal',
-            'enableClientValidation' => true,
-            'errorSummaryCssClass' => 'error-summary alert alert-danger',
-            'fieldConfig' => [
-                'template' => "{label}\n{beginWrapper}\n{input}\n{hint}\n{error}\n{endWrapper}",
-                'horizontalCssClasses' => [
-                    'label' => 'col-sm-2',
-                    #'offset' => 'col-sm-offset-4',
-                    'wrapper' => 'col-sm-8',
-                    'error' => '',
-                    'hint' => '',
-                ],
-            ],
-    ]);
+    <!-- attribute subtype_id -->
+    <?=
+        $form
+        ->field($model, 'subtype_id')
+        ->dropDownList(\app\models\InstalationSubtype::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
     ?>
 
-    <div class="">
-        <?php $this->beginBlock('main'); ?>
+    <!-- attribute ownership_status -->
+    <?=
+        $form
+        ->field($model, 'ownership_status')
+        ->dropDownList(\app\dictionaries\OwnershipStatus::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
+    ?>
 
-        <p>
+    <!-- attribute voltage_id -->
+    <?=
+        $form
+        ->field($model, 'voltage_id')
+        ->dropDownList(\app\models\Voltage::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
+    ?>
 
-            <!-- attribute subtype_id -->
-            <?=
-                $form
-                ->field($model, 'subtype_id')
-                ->dropDownList(\app\models\InstalationSubtype::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
-            ?>
+    <!-- attribute substation_quantity -->
+    <?= $form->field($model, 'substation_quantity')->textInput() ?>
 
-            <!-- attribute ownership_status -->
-            <?=
-                $form
-                ->field($model, 'ownership_status')
-                ->dropDownList(\app\dictionaries\OwnershipStatus::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
-            ?>
+    <!-- attribute panel_quantity -->
+    <?= $form->field($model, 'panel_quantity')->textInput() ?>
 
-            <!-- attribute voltage_id -->
-            <?=
-                $form
-                ->field($model, 'voltage_id')
-                ->dropDownList(\app\models\Voltage::allMap(), ['prompt' => Yii::t('cruds', 'Select'),]);
-            ?>
+    <!-- attribute jtm_length_kms -->
+    <?= $form->field($model, 'jtm_length_kms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute substation_quantity -->
-            <?= $form->field($model, 'substation_quantity')->textInput() ?>
+    <!-- attribute sktm_length_ms -->
+    <?= $form->field($model, 'sktm_length_ms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute panel_quantity -->
-            <?= $form->field($model, 'panel_quantity')->textInput() ?>
+    <!-- attribute sutm_length_ms -->
+    <?= $form->field($model, 'sutm_length_ms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute jtm_length_kms -->
-            <?= $form->field($model, 'jtm_length_kms')->textInput(['maxlength' => true]) ?>
+    <!-- attribute jtr_length_kms -->
+    <?= $form->field($model, 'jtr_length_kms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute sktm_length_ms -->
-            <?= $form->field($model, 'sktm_length_ms')->textInput(['maxlength' => true]) ?>
+    <!-- attribute sktr_length_ms -->
+    <?= $form->field($model, 'sktr_length_ms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute sutm_length_ms -->
-            <?= $form->field($model, 'sutm_length_ms')->textInput(['maxlength' => true]) ?>
+    <!-- attribute sutr_length_ms -->
+    <?= $form->field($model, 'sutr_length_ms')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute jtr_length_kms -->
-            <?= $form->field($model, 'jtr_length_kms')->textInput(['maxlength' => true]) ?>
+    <!-- attribute substation_capacity_kva -->
+    <?= $form->field($model, 'substation_capacity_kva')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute sktr_length_ms -->
-            <?= $form->field($model, 'sktr_length_ms')->textInput(['maxlength' => true]) ?>
+    <!-- attribute short_circuit_capacity_a -->
+    <?= $form->field($model, 'short_circuit_capacity_a')->textInput(['maxlength' => true]) ?>
 
-			<!-- attribute sutr_length_ms -->
-            <?= $form->field($model, 'sutr_length_ms')->textInput(['maxlength' => true]) ?>
-
-			<!-- attribute substation_capacity_kva -->
-            <?= $form->field($model, 'substation_capacity_kva')->textInput(['maxlength' => true]) ?>
-
-			<!-- attribute short_circuit_capacity_a -->
-            <?= $form->field($model, 'short_circuit_capacity_a')->textInput(['maxlength' => true]) ?>
-
-			<!-- attribute distribution_region -->
-            <?= $form->field($model, 'distribution_region')->textInput(['maxlength' => true]) ?>
-
-        </p>
-
-        <?php $this->endBlock(); ?>
-
-        <?=
-        Tabs::widget([
-            'encodeLabels' => false,
-            'items' => [
-                [
-                    'label' => $model->aliasModel,
-                    'content' => $this->blocks['main'],
-                    'active' => true,
-                ],
-            ],
-        ]);
-        ?>
-
-        <hr/>
-
-        <?= $form->errorSummary($model); ?>
-
-        <?=
-        Html::submitButton(
-            '<span class="glyphicon glyphicon-check"></span> ' .
-            ($model->isNewRecord ? Yii::t('cruds', 'Create') : Yii::t('cruds', 'Save')),
-            [
-            'id' => 'save-'.$model->formName(),
-            'class' => 'btn btn-success'
-            ]
-        );
-        ?>
-
-        </div>
-
-    <?php ActiveForm::end(); ?>
+    <!-- attribute distribution_region -->
+    <?= $form->field($model, 'distribution_region')->textInput(['maxlength' => true]) ?>
 
 </div>
-
