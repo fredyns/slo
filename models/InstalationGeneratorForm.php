7aaf561023025905ce9fb90815b160d669767e02
Fredy Nurman Saleh <email@fredyns.net>
2018-12-18 08:16:44 +0700

    build form rules

--- models/InstalationGeneratorForm.php
+++ models/InstalationGeneratorForm.php
@@ -19,10 +19,9 @@ class InstalationGeneratorForm extends InstalationGenerator
     public function behaviors()
     {
         return ArrayHelper::merge(
-            parent::behaviors(),
-            [
+                parent::behaviors(), [
                 # custom behaviors
-            ]
+                ]
         );
     }
 
@@ -33,33 +32,55 @@ class InstalationGeneratorForm extends InstalationGenerator
     {
         return [
             # filter
-            /*//
             'string_filter' => [
-                ['name'],
+                [
+                    'capacity_unit', 'test_capacity_unit',
+                    'unit_number',
+                    'turbine_serial_number', 'generator_serial_number',
+                    'each_module_capacity', 'each_inverter_capacity',
+                    'calorific_value', 'fuel_consumption_hhv', 'fuel_consumption_lhv', 'sfc',
+                ],
                 'filter',
                 'filter' => function($value){
                     return StringCleaner::forPlaintext($value);
-                },
+              },
             ],
-            //*/
             # default
             # required
-            # type
-            # format
-            # option
-            # constraint
-            # safe
             [['submission_id'], 'required'],
+            # type
             [['submission_id', 'subtype_id', 'fuel_id', 'module_quantity', 'inverter_quantity', 'calorific_value_file_id', 'fuel_consumption_rate_file_id'], 'integer'],
             [['capacity', 'test_capacity'], 'number'],
             [['capacity_unit', 'test_capacity_unit'], 'string', 'max' => 8],
             [['unit_number'], 'string', 'max' => 32],
             [['turbine_serial_number', 'generator_serial_number', 'each_module_capacity', 'each_inverter_capacity', 'calorific_value', 'fuel_consumption_hhv', 'fuel_consumption_lhv', 'sfc'], 'string', 'max' => 64],
             [['unit'], 'string', 'max' => 128],
+            # format
+            # restriction
             [['submission_id'], 'unique'],
-            [['fuel_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Fuel::className(), 'targetAttribute' => ['fuel_id' => 'id']],
-            [['submission_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Submission::className(), 'targetAttribute' => ['submission_id' => 'id']],
-            [['subtype_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\InstalationSubtype::className(), 'targetAttribute' => ['subtype_id' => 'id']],
+            # constraint
+            [
+                ['fuel_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => \app\models\Fuel::className(),
+                'targetAttribute' => ['fuel_id' => 'id'],
+            ],
+            [
+                ['submission_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => \app\models\Submission::className(),
+                'targetAttribute' => ['submission_id' => 'id'],
+            ],
+            [
+                ['subtype_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => \app\models\InstalationSubtype::className(),
+                'targetAttribute' => ['subtype_id' => 'id'],
+            ],
+            # safe   
         ];
     }
 
