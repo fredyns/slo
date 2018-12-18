2206503b73f10b94fa81442bf6ab0d7f662587cd
Fredy Nurman Saleh <email@fredyns.net>
2018-12-18 09:38:16 +0700

    build basic rules & labels

--- models/InstalationTransmission.php
+++ models/InstalationTransmission.php
@@ -14,16 +14,16 @@ use yii\helpers\ArrayHelper;
  */
 class InstalationTransmission extends BaseInstalationTransmission
 {
+
     /**
      * @inheritdoc
      */
     public function behaviors()
     {
         return ArrayHelper::merge(
-            parent::behaviors(),
-            [
+                parent::behaviors(), [
                 # custom behaviors
-            ]
+                ]
         );
     }
 
@@ -34,35 +34,37 @@ class InstalationTransmission extends BaseInstalationTransmission
     {
         return [
             # filter
-            /*//
             'string_filter' => [
-                ['name'],
+                [
+                    'jtet', 'jtt',
+                    'power_house_capacity', 'tower',
+                    'line_bay', 'bus_coupler_bay', 'transformer_bay', 'power_breaker_capacity', 'power_transformer_capacity',
+                ],
                 'filter',
                 'filter' => function($value){
                     return StringCleaner::forPlaintext($value);
-                },
+              },
             ],
-            //*/
             # default
             # required
-            # type
-            # format
-            # option
-            # constraint
-            # safe
             [['submission_id'], 'required'],
+            # type
             [['submission_id', 'subtype_id', 'ownership_status', 'network_id', 'voltage_id'], 'integer'],
             [['jtet', 'jtt'], 'string', 'max' => 128],
             [['power_house_capacity'], 'string', 'max' => 32],
             [['tower', 'line_bay', 'bus_coupler_bay', 'transformer_bay', 'power_breaker_capacity', 'power_transformer_capacity'], 'string', 'max' => 64],
+            # format
+            # restriction
             [['submission_id'], 'unique'],
+            # constraint
             [['network_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\TransmissionNetwork::className(), 'targetAttribute' => ['network_id' => 'id']],
             [['submission_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Submission::className(), 'targetAttribute' => ['submission_id' => 'id']],
             [['subtype_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\InstalationSubtype::className(), 'targetAttribute' => ['subtype_id' => 'id']],
             [['voltage_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Voltage::className(), 'targetAttribute' => ['voltage_id' => 'id']],
+            # safe
         ];
     }
-    
+
     /**
      * Alias name of table for crud views Lists all models.
      * Change the alias name manual if needed later
@@ -87,8 +89,8 @@ class InstalationTransmission extends BaseInstalationTransmission
             'subtype_id' => Yii::t('models', 'Subtype'),
             'ownership_status' => Yii::t('models', 'Ownership Status'),
             'network_id' => Yii::t('models', 'Network'),
-            'jtet' => Yii::t('models', 'Jtet'),
-            'jtt' => Yii::t('models', 'Jtt'),
+            'jtet' => Yii::t('models', 'JTET'),
+            'jtt' => Yii::t('models', 'JTT'),
             'voltage_id' => Yii::t('models', 'Voltage'),
             'power_house_capacity' => Yii::t('models', 'Power House Capacity'),
             'tower' => Yii::t('models', 'Tower'),
@@ -99,4 +101,5 @@ class InstalationTransmission extends BaseInstalationTransmission
             'power_transformer_capacity' => Yii::t('models', 'Power Transformer Capacity'),
         ];
     }
+
 }
