3d5c329e1950a4a3b79d100c5a5e04fb401c9a29
Fredy Nurman Saleh <email@fredyns.net>
2018-12-18 13:37:08 +0700

    instalation_transmission form rules

--- models/InstalationDistributionForm.php
+++ models/InstalationDistributionForm.php
@@ -19,10 +19,9 @@ class InstalationDistributionForm extends InstalationDistribution
     public function behaviors()
     {
         return ArrayHelper::merge(
-            parent::behaviors(),
-            [
+                parent::behaviors(), [
                 # custom behaviors
-            ]
+                ]
         );
     }
 
@@ -33,29 +32,30 @@ class InstalationDistributionForm extends InstalationDistribution
     {
         return [
             # filter
-            /*//
             'string_filter' => [
-                ['name'],
+                ['distribution_region'],
                 'filter',
                 'filter' => function($value){
                     return StringCleaner::forPlaintext($value);
                 },
             ],
-            //*/
             # default
             # required
             # type
-            # format
-            # option
-            # constraint
-            # safe
-            [['submission_id'], 'required'],
-            [['submission_id', 'subtype_id', 'ownership_status', 'voltage_id', 'substation_quantity', 'panel_quantity'], 'integer'],
+            [['subtype_id', 'ownership_status', 'voltage_id', 'substation_quantity', 'panel_quantity'], 'integer'],
             [['jtm_length_kms', 'sktm_length_ms', 'sutm_length_ms', 'jtr_length_kms', 'sktr_length_ms', 'sutr_length_ms', 'substation_capacity_kva', 'short_circuit_capacity_a'], 'number'],
             [['distribution_region'], 'string', 'max' => 4],
-            [['submission_id'], 'unique'],
-            [['submission_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Submission::className(), 'targetAttribute' => ['submission_id' => 'id']],
-            [['subtype_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\InstalationSubtype::className(), 'targetAttribute' => ['subtype_id' => 'id']],
+            # format
+            # restriction
+            # constraint
+            [
+                ['subtype_id'],
+                'exist',
+                'skipOnError' => true,
+                'targetClass' => \app\models\InstalationSubtype::className(),
+                'targetAttribute' => ['subtype_id' => 'id'],
+            ],
+            # safe
         ];
     }
 
