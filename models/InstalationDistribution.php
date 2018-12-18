713a6212bf9413a77b259cf09998be709ebfef00
Fredy Nurman Saleh <email@fredyns.net>
2018-12-18 13:32:40 +0700

    instalation_transmission base rules & labels

--- models/InstalationDistribution.php
+++ models/InstalationDistribution.php
@@ -14,16 +14,16 @@ use yii\helpers\ArrayHelper;
  */
 class InstalationDistribution extends BaseInstalationDistribution
 {
+
     /**
      * @inheritdoc
      */
     public function behaviors()
     {
         return ArrayHelper::merge(
-            parent::behaviors(),
-            [
+                parent::behaviors(), [
                 # custom behaviors
-            ]
+                ]
         );
     }
 
@@ -34,32 +34,30 @@ class InstalationDistribution extends BaseInstalationDistribution
     {
         return [
             # filter
-            /*//
             'string_filter' => [
-                ['name'],
+                ['distribution_region'],
                 'filter',
                 'filter' => function($value){
                     return StringCleaner::forPlaintext($value);
                 },
             ],
-            //*/
             # default
             # required
-            # type
-            # format
-            # option
-            # constraint
-            # safe
             [['submission_id'], 'required'],
+            # type
             [['submission_id', 'subtype_id', 'ownership_status', 'voltage_id', 'substation_quantity', 'panel_quantity'], 'integer'],
             [['jtm_length_kms', 'sktm_length_ms', 'sutm_length_ms', 'jtr_length_kms', 'sktr_length_ms', 'sutr_length_ms', 'substation_capacity_kva', 'short_circuit_capacity_a'], 'number'],
             [['distribution_region'], 'string', 'max' => 4],
+            # format
+            # restriction
             [['submission_id'], 'unique'],
+            # constraint
             [['submission_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\Submission::className(), 'targetAttribute' => ['submission_id' => 'id']],
             [['subtype_id'], 'exist', 'skipOnError' => true, 'targetClass' => \app\models\InstalationSubtype::className(), 'targetAttribute' => ['subtype_id' => 'id']],
+            # safe
         ];
     }
-    
+
     /**
      * Alias name of table for crud views Lists all models.
      * Change the alias name manual if needed later
@@ -84,17 +82,18 @@ class InstalationDistribution extends BaseInstalationDistribution
             'subtype_id' => Yii::t('models', 'Subtype'),
             'ownership_status' => Yii::t('models', 'Ownership Status'),
             'distribution_region' => Yii::t('models', 'Distribution Region'),
-            'jtm_length_kms' => Yii::t('models', 'Jtm Length Kms'),
-            'sktm_length_ms' => Yii::t('models', 'Sktm Length Ms'),
-            'sutm_length_ms' => Yii::t('models', 'Sutm Length Ms'),
-            'jtr_length_kms' => Yii::t('models', 'Jtr Length Kms'),
-            'sktr_length_ms' => Yii::t('models', 'Sktr Length Ms'),
-            'sutr_length_ms' => Yii::t('models', 'Sutr Length Ms'),
-            'substation_capacity_kva' => Yii::t('models', 'Substation Capacity Kva'),
+            'jtm_length_kms' => Yii::t('models', 'JTM Length (kms)'),
+            'sktm_length_ms' => Yii::t('models', 'SKTM Length (ms)'),
+            'sutm_length_ms' => Yii::t('models', 'SUTM Length (ms)'),
+            'jtr_length_kms' => Yii::t('models', 'JTR Length (ms)'),
+            'sktr_length_ms' => Yii::t('models', 'SKTR Length (ms)'),
+            'sutr_length_ms' => Yii::t('models', 'SUTR Length (ms)'),
+            'substation_capacity_kva' => Yii::t('models', 'Substation Capacity (kVA)'),
             'voltage_id' => Yii::t('models', 'Voltage'),
             'substation_quantity' => Yii::t('models', 'Substation Quantity'),
             'panel_quantity' => Yii::t('models', 'Panel Quantity'),
-            'short_circuit_capacity_a' => Yii::t('models', 'Short Circuit Capacity A'),
+            'short_circuit_capacity_a' => Yii::t('models', 'Short Circuit Capacity (A)'),
         ];
     }
+
 }
